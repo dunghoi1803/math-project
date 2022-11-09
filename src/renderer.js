5a3fae471be7cce9fb3227f5da036cb0edea2f3c
Dung Nguyen <dunggoban@gmail.com>
2022-11-09 11:01:55 +0700

    addition of the third quest

--- src/renderer.js
+++ src/renderer.js
@@ -1,18 +1,22 @@
 const { ipcRenderer } = require("electron");
+const { sqrt } = require("mathjs");
 const math = require("mathjs");
 
 function run() {
-var n = document.getElementById('n').value;
-var sum_Xi = document.getElementById('sum_Xi').value;
-var sum_Zi = document.getElementById('sum_Zi').value;
-var sum_Yi = document.getElementById('sum_Yi').value;
-var sum_Xi_sqr = document.getElementById('sum_Xi_sqr').value;
-var sum_Zi_sqr = document.getElementById('sum_Zi_sqr').value;
-var sum_Yi_sqr = document.getElementById('sum_Yi_sqr').value;
-var sum_XiZi = document.getElementById('sum_XiZi').value;
-var sum_YiZi = document.getElementById('sum_YiZi').value;
-var sum_XiYi = document.getElementById('sum_XiYi').value;
-var k = document.getElementById('k').value;
+let n = document.getElementById('n').value;
+let sum_Xi = document.getElementById('sum_Xi').value;
+let sum_Zi = document.getElementById('sum_Zi').value;
+let sum_Yi = document.getElementById('sum_Yi').value;
+let sum_Xi_sqr = document.getElementById('sum_Xi_sqr').value;
+let sum_Zi_sqr = document.getElementById('sum_Zi_sqr').value;
+let sum_Yi_sqr = document.getElementById('sum_Yi_sqr').value;
+let sum_XiZi = document.getElementById('sum_XiZi').value;
+let sum_YiZi = document.getElementById('sum_YiZi').value;
+let sum_XiYi = document.getElementById('sum_XiYi').value;
+let k = document.getElementById('k').value;
+let Xo = document.getElementById('Xo').value;
+let Zo = document.getElementById('Zo').value;
+let hằng_số_t_alpha = document.getElementById('hằng_số_t_alpha').value;
 
 const matrix1 = [[n, sum_Xi ,sum_Zi ], [sum_Xi ,sum_Xi_sqr ,sum_XiZi ], [sum_Zi ,sum_XiZi , sum_Zi_sqr ]]
 const matrix_XtX = math.matrix(matrix1)    
@@ -31,19 +35,29 @@ const inverse = (m) => {
 
   document.getElementById('result1').innerHTML = betaMu
 
-
-var sum_resid_sqr = sum_Yi_sqr - (betaMu[0]*sum_Yi + betaMu[1]*sum_XiYi + betaMu[2]*sum_YiZi);
-var sigmaMu_sqr = sum_resid_sqr/(n-k);
+let sum_resid_sqr = sum_Yi_sqr - (betaMu.subset(math.index(0))*sum_Yi + betaMu.subset(math.index(1))*sum_XiYi + betaMu.subset(math.index(2))*sum_YiZi);
+let sigmaMu_sqr = sum_resid_sqr/(n-k);
 const khoảng_tin_cậy_betaMu = []
 const standardError_array = []
 
 for (j = 1; j < betaMu.length; j++){
-    var variance_betaMu = sigmaMu_sqr*(matrix_XtX_inverse[j][j])
-    var standardError = Math.sqrt(variance_betaMu)
+    let variance_betaMu = sigmaMu_sqr*(matrix_XtX_inverse[j][j])
+    let standardError = Math.sqrt(variance_betaMu)
     standardError_array.push(standardError)
-    var KTC_left = betaMu - 2.626*standardError
-    var KTC_right = betaMu + 2.626*standardError
+    let KTC_left = betaMu - hằng_số_t_alpha*standardError
+    let KTC_right = betaMu + hằng_số_t_alpha*standardError
     khoảng_tin_cậy_betaMu.push(KTC_left + '-' + KTC_right)
 }
-document.getElementById('result2').innerHTML = khoảng_tin_cậy_betaMu
+document.getElementById('result2').innerHTML = sigmaMu_sqr
+
+const matrix_Xto = math.matrix([1, Xo, Zo])
+const matrix_Xo = math.matrix([[1], [Xo], [Zo]])
+let Yo = betaMu.subset(math.index(0)) + betaMu.subset(math.index(1))*Xo + betaMu.subset(math.index(2))*Zo
+let variance_YoMu = sigmaMu_sqr*math.multiply(math.multiply(matrix_Xto, matrix_XtX_inverse), matrix_Xo)
+let standardError_YoMu = Math.sqrt(variance_YoMu)
+let KTC_left_Yo = Yo - hằng_số_t_alpha*standardError_YoMu;
+let KTC_right_Yo = Yo + hằng_số_t_alpha*standardError_YoMu;
+const KTC_Y_Xo = [KTC_left_Yo, KTC_right_Yo]
+
+document.getElementById('result3').innerHTML = Yo 
 }
